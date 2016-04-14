bee4d45fe35da5da3abbe6a1ca31d56fcd116039
maaaaagda <magdapolak96@gmail.com>
2016-04-14 15:11:29 +0200

    Inne klasy dodane

---
+++ src/.idea/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Wyniki w = new Wyniki();
+
+        System.out.println(w.srednieWyniki(5, 20, 50));
+       }
+}
